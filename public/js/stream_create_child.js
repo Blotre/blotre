26d18fbe0e6c1d525bc8921b52600c8218acd77e
Matt Bierner <mattbierner@gmail.com>
2015-03-29 00:26:53 -0700

    Spinning create child indicator while working.

--- public/js/stream_create_child.js
+++ public/js/stream_create_child.js
@@ -20,17 +20,44 @@ var getTarget = function() {
     return ((path && path[1]) || '');
 };
 
+var lockButton = function(sel) {
+     sel
+        .prop("disabled", true)
+        .children('.glyphicon')
+            .addClass('glyphicon-refresh glyphicon-refresh-animate');
+};
+
+var unlockButton = function(sel) {
+    sel
+       .prop("disabled", false)
+       .children('.glyphicon')
+           .removeClass('glyphicon-refresh  glyphicon-refresh-animate');
+};
+
 $(function(){
     var model = new AppViewModel(
         application_model.initialUser());
 
     $('.create-child-button').click(function(e) {
+        var btn = $(this);
+        lockButton(btn);
+
         $.ajax({
             type: "PUT",
-            url: jsRoutes.controllers.Stream.createChildStream(getTarget()).url
+            url: jsRoutes.controllers.Stream.createChildStream(getTarget()).url,
+            headers: {
+                Accept: "application/json"
+            },
+            error: function(e) {
+                unlockButton(btn);
+            }
         })
         .then(function(result) {
-            debugger;
+            if (result && !result.error) {
+                document.location.href = jsRoutes.controllers.Stream.getStream(result.uri).url;
+            } else {
+                unlockButton(btn);
+            }
         });
     });
 
